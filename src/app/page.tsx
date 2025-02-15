9665891c262d2ee122c0e0f0861812208a2a9972
hunter-nguyen <hunternguyen8@gmail.com>
2025-02-15 12:05:44 -0800

    add landing page and routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,11 @@ import signOut from "@/supabase/auth/signOut";
 import { useAuthContext } from "@/context/AuthContext";
 import MapLayout from "@/components/map-layout"
 import Link from 'next/link';
+import LandingPage from "./components/LandingPage";
 
 export default function Home() {
   const { user } = useAuthContext() as { user: any };
   return (
-/*     <div className="flex flex-col">
-      {user && user.email}
-      <button onClick={signInWithGoogle}>
-        sign in
-      </button>
-      <button onClick={signOut}>
-        sign out
-      </button>
-    <div> */
-    <div>
-      <MapLayout />
-      <Link 
-        href="/report" 
-        className="fixed bottom-8 right-8 bg-blue-500 text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-600 transition-colors"
-      >
-        Report Incident
-      </Link>
-    </div>
+    <LandingPage/>
   );
 }
