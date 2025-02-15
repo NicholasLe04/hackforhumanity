7458982822cc356b7e8380645d1381ca20064c40
Steven Le <stevenleusa79@yahoo.com>
2025-02-15 11:50:52 -0800

    upload modifed + shadcn

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -74,11 +74,13 @@ export default function Map() {
   }, [])
 
   return (
-    <div
-      ref={mapContainer}
-      className="absolute inset-0"
-      style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}
-    />
+    <div>
+      <div
+        ref={mapContainer}
+        className="absolute inset-0"
+        style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}
+      />
+    </div>
   )
 }
 
