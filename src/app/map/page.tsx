e0fe26d3eec7fffc27e0436ff257298c43b95b9f
Steven Le <stevenleusa79@yahoo.com>
2025-02-15 12:44:16 -0800

    made upload button absolute, ui will change soon anyway

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -12,8 +12,8 @@ export default function MapPage() {
   const { user } = useAuthContext() as { user: User };
   
   return (
-    <div>
-      <div className="flex flex-col">
+    <div className="relative h-[100vh]">
+      <div className="flex flex-col absolute z-10 bg-black">
         {user && user.email}
         <button onClick={signInWithGoogle}>
           sign in
@@ -22,15 +22,13 @@ export default function MapPage() {
           sign out
         </button>
       </div>
-      <div className="relative">
-        <MapLayout />
-        <Link href="/report" className="absolute bottom-6 right-6">
-          <Button className="h-14 w-14 rounded-full shadow-lg border-2 border-black bg-white hover:bg-black/10 transition-colors duration-200">
-            <Plus className="h-6 w-6 text-black" />
-            <span className="sr-only">Add accident report</span>
-          </Button>
-        </Link>
-      </div>
+      <MapLayout />
+      <Link href="/report" className="absolute bottom-6 right-6">
+        <Button className="h-14 w-14 rounded-full shadow-lg border-2 border-black bg-white hover:bg-black/10 transition-colors duration-200">
+          <Plus className="h-6 w-6 text-black" />
+          <span className="sr-only">Add accident report</span>
+        </Button>
+      </Link>
     </div>
   )
 }
