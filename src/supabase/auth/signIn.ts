94a924131eaab431b85ffadb8f74df8fad20169a
NicholasLe04 <nicholasl.le224@gmail.com>
2025-02-15 11:34:59 -0800

    Add Auth

---
+++ src/supabase/auth/signIn.ts
@@ -0,0 +1,19 @@
+import { supabase } from "../app";
+
+// Function to sign in with email and password
+export default async function signInWithGoogle() {
+    let result = null;
+    let error = null;
+
+    try {
+        result = await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                redirectTo: "https://ryprhnikjykoacktfnkn.supabase.co/auth/v1/callback"
+            }
+        })
+    } catch (e) {
+        error = e;
+    }
+    return { result, error };
+}
---
+++ src/supabase/auth/signOut.ts
@@ -0,0 +1,14 @@
+import { supabase } from "../app";
+
+// Function to sign in with email and password
+export default async function signOut() {
+    let result = null;
+    let error = null;
+
+    try {
+        result = await supabase.auth.signOut();
+    } catch (e) {
+        error = e;
+    }
+    return { result, error };
+}
