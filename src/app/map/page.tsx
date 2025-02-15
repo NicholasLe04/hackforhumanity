1e4f1ebc3e7c300656ca72dac3e325420c17cc05
Steven Le <stevenleusa79@yahoo.com>
2025-02-15 13:05:26 -0800

    cleanup

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 
-import MapLayout from "@/components/map-layout"
 import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
 import Link from "next/link"
+import { useState } from "react"
+import Map from "@/components/map"
+import Sidebar from "@/components/sidebar"
 
 export default function MapPage() {
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(true)
+
   return (
-    <div className="relative h-[100vh]">
-      <MapLayout />
+    <div className="flex h-[100vh] w-full overflow-hidden">
+      <Sidebar isExpanded={isSidebarExpanded} setIsExpanded={setIsSidebarExpanded} />
+      <div className="flex-1 relative">
+        <Map />
+      </div>
       <Link href="/report" className="absolute bottom-6 right-6">
         <Button className="h-14 w-14 rounded-full shadow-lg border-2 border-black bg-white hover:bg-black/10 transition-colors duration-200">
           <Plus className="h-6 w-6 text-black" />
